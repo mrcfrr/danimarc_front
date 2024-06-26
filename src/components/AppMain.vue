80511ad162bca5499371f372d87134e2578c4a9f
Alla <>
2024-06-26 11:38:16 +0200

    folder-structure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,44 +1,74 @@
 <template>
     <main>
         <div>
-        <h1>Documenti</h1>
         <ul>
-            <li v-for="document in data" :key="document">
-                <div v-if="isImage(document)">
-                    <img :src="getDocumentUrl(document)" :alt="document" width="200" />
-                </div>
-                <div v-else-if="isPdf(document)">
-                    <a :href="getDocumentUrl(document)" target="_blank">View PDF: {{ document }}</a>
-                </div>
-                <div v-else>
-                    {{ document }}
-                </div>
-            </li>
+            <template v-for="item in data">
+                <li v-if="item.type === 'directory'" :key="item.path">
+                    <span @click="toggleFolder(item.path)" >
+                        {{ item.name }}
+                        <span v-if="isOpen(item.path)" >[-]</span>
+                        <span v-else>[+]</span>
+                    </span>
+                    <ul v-if="isOpen(item.path)">
+                        <app-main :data="item.contents" />
+                    </ul>
+                </li>
+                <li v-if="item.type === 'file'" :key="item.path">
+                    <div v-if="isImage(item.path)">
+                        <img :src="getDocumentUrl(item.path)" :alt="item.name" width="200">
+                    </div>
+                    <div v-else-if="isPdf(item.path)">
+                        <a :href="getDocumentUrl(item.path)" target="_blank">Apri PDF: {{ item.name }}</a>
+                    </div>
+                    <div v-else>
+                        {{ item.name }}
+                    </div>
+                </li>
+            </template>
         </ul>
     </div>
     </main>
 </template>
 
 <script>
-export default {
-    name:'AppMain',
-    props: {
-        data: Array
-    },
-    methods: {
-        isImage(file) {
-            const extension = file.split('.').pop().toLowerCase();
-            return ['jpg', 'jpeg', 'png'].includes(extension);
+    export default {
+        name:'AppMain',
+        props: {
+            data: Array
         },
-        isPdf(file) {
-            const extension = file.split('.').pop().toLowerCase();
-            return extension === 'pdf';
+        data(){
+            return {
+                openFolders: []
+            };
         },
-        getDocumentUrl(document) {
-            return `http://127.0.0.1:8000/api/documents/${encodeURIComponent(document)}`;
+        methods: {
+            isImage(file) {
+                const extension = file.split('.').pop().toLowerCase();
+                return ['jpg', 'jpeg', 'png'].includes(extension);
+            },
+
+            isPdf(file) {
+                const extension = file.split('.').pop().toLowerCase();
+                return extension === 'pdf';
+            },
+
+            getDocumentUrl(document) {
+                return `http://127.0.0.1:8000/api/documents/${encodeURIComponent(document)}`;
+            },
+
+            toggleFolder(path){
+                if(this.isOpen(path)){
+                    this.openFolders = this.openFolders.filter(folder => folder !== path);
+                } else {
+                    this.openFolders.push(path);
+                }
+            },
+
+            isOpen(path){
+                return this.openFolders.includes(path);
+            }
         }
     }
-}
 </script>
 
 <style>
